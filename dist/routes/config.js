2edda6c148dcf753f521063a1eb9a3d9ea648a63
ficool2 <34815548+ficool2@users.noreply.github.com>
2024-05-08 17:48:18 +0100

    Fix rpID again

--- dist/routes/config.js
+++ dist/routes/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     rpName: 'Biometric FingerPrint Login',
-    rpID: 'biometricfingerprintlogin',
+    rpID: 'mern-video.azurewebsites.net',
     origin: 'https://mern-video.azurewebsites.net',
     baseURL: 'https://mern-video.azurewebsites.net'
 };
--- src/routes/config.ts
+++ src/routes/config.ts
@@ -1,6 +1,6 @@
 const config = {
   rpName: 'Biometric FingerPrint Login',
-  rpID: 'biometricfingerprintlogin',
+  rpID: 'mern-video.azurewebsites.net',
   origin: 'https://mern-video.azurewebsites.net',
   baseURL: 'https://mern-video.azurewebsites.net'
 };
