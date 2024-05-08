d4366b02226927903e64251eff67f80d10a4372c
ficool2 <34815548+ficool2@users.noreply.github.com>
2024-05-08 16:27:47 +0100

    Fix server URL

--- dist/routes/config.js
+++ dist/routes/config.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     rpName: 'Biometric FingerPrint Login',
     rpID: 'localhost',
-    origin: 'http://localhost:3000',
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://mern-video.azurewebsites.net' : 'http://localhost:3000'
+    origin: 'https://mern-video.azurewebsites.net',
+    baseURL: 'https://mern-video.azurewebsites.net'
 };
 exports.default = config;
