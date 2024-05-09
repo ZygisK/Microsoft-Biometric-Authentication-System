5bcc9523bfcad5d9d6be8d8ee8eec33d92df1404
ficool2 <34815548+ficool2@users.noreply.github.com>
2024-05-09 10:12:12 +0100

    Add debugging logout alert

--- dist/public/js/javascripts.js
+++ dist/public/js/javascripts.js
@@ -147,6 +147,10 @@ async function getUserInfo() {
       document.getElementById('success').innerText = '';
       document.getElementById('loginname').innerText = '';
     }
+	else
+	{
+		alert("Failed to logout: " + json.statusText);
+	}
   }
 
   fetch('/result', {
